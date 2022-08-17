71a9b319494b91b3546e08ea2b70c832d00870b2
Quentin Almira <quentin.almira@gmail.com>
2022-08-17 17:05:50 +0200

    Regex Email V1

--- js/modal.js
+++ js/modal.js
@@ -13,6 +13,8 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalCross = document.querySelector(".close");
 
+
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -29,6 +31,58 @@ function closeModal() {
 
 // close modal event
 modalCross.addEventListener("click", closeModal);
+
+// **********************************
+// Form validation RegExp
+// **********************************
+
+// *****Email*****
+
+let form = document.querySelector('#loginForm');
+
+console.log(form.email);
+
+//Ecouter la modification de l'e-mail
+
+form.email.addEventListener('change', function() {
+    validEmail(this);
+});
+
+// *********Validation Email********************
+
+const validEmail = function(inputEmail) {
+
+  //création de la regex pour la validation email
+
+  let emailRegExp = new RegExp(
+      '^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g'
+  );
+
+  let testEmail = emailRegExp.test(inputEmail.value);
+
+
+  // Récupération de la balise small
+
+  let small = inputEmail.nextElementSibling;
+
+    // Test de la regex
+
+  if(testEmail){
+      small.innerHTML="Adresse Valide";
+      small.classList.remove('text-danger');
+      small.classList.add('text-success');
+      small.style.color="#00ff00"
+  }
+
+  else{
+      small.innerHTML="Adresse non valide";
+      small.classList.remove('text-success');
+      small.classList.add('text-danger');
+      
+  }
+
+};
+
 
 
 
