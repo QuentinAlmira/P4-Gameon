3b32da73dad8d30a1a38090d7193e40095cbb441
Quentin Almira <quentin.almira@gmail.com>
2022-08-18 17:14:50 +0200

    RegExp first/lastname v2

--- js/modal.js
+++ js/modal.js
@@ -68,10 +68,9 @@ const validEmail = function(inputEmail) {
     // Test de la regex
 
   if(testEmail){
-      small.innerHTML="Adresse Valide";
+      small.innerHTML="Adresse mail valide";
       small.classList.remove('text-danger');
       small.classList.add('text-success');
-      small.style.color="#00ff00"
   }
 
   else{
@@ -83,7 +82,107 @@ const validEmail = function(inputEmail) {
 
 };
 
+// *********Validation Prénon ********************
 
+//Ecouter la modification prénom
+
+form.first.addEventListener('change', function() {
+  validFirst(this);
+});
+
+const validFirst = function(inputFirst) {
+
+  let msg;
+  let valid = false;
+
+  //Au moins 2 caracteres
+
+  if (inputFirst.value.length < 2) {
+      msg = 'le prénom doit contenir au moins 2 caractères';
+  }
+
+  //Mdp Valide
+
+  else{
+      msg ='le prénom est valide';
+      valid = true;
+  }
+
+  //affichage
+
+      // Récupération de la balise small
+
+      let small = inputFirst.nextElementSibling;
+
+      // Test de la regex
+
+    if(valid){
+        small.innerHTML="Prénom valide";
+        small.classList.remove('text-danger');
+        small.classList.add('text-success');
+        return true;
+
+    }
+
+    else{
+        small.innerHTML = msg;
+        small.classList.remove('text-success');
+        small.classList.add('text-danger');
+        return false;
+    }
+
+};
+
+// *********Validation Nom ********************
+
+//Ecouter la modification prénom
+
+form.last.addEventListener('change', function() {
+  validLast(this);
+});
+
+const validLast = function(inputLast) {
+
+  let msg;
+  let valid = false;
+
+  //Au moins 2 caracteres
+
+  if (inputLast.value.length < 2) {
+      msg = 'le Nom doit contenir au moins 2 caractères';
+  }
+
+  //Mdp Valide
+
+  else{
+      msg ='le Nom est valide';
+      valid = true;
+  }
+
+  //affichage
+
+      // Récupération de la balise small
+
+      let small = inputLast.nextElementSibling;
+
+      // Test de la regex
+
+    if(valid){
+        small.innerHTML="Nom valide";
+        small.classList.remove('text-danger');
+        small.classList.add('text-success');
+        return true;
+
+    }
+
+    else{
+        small.innerHTML = msg;
+        small.classList.remove('text-success');
+        small.classList.add('text-danger');
+        return false;
+    }
+
+};
 
 
 
